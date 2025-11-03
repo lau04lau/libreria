acbaa0dc8c3c0998ab82715a7bc2bc709cd2ba84
Laura Vogel <laura.vogel@davinci.edu.ar>
2025-11-03 19:40:40 -0300

    Test agregado

--- libreria/libro.py
+++
@@ -1,9 +0,0 @@
-class libro:
-    def __init__(self, id, titulo, precio, stock):
-        self.id=id
-        self.titulo=titulo
-        self.precio=precio
-        self.stock=stock
-
-def __str__(self):
-        return "titulo {self.titulo} \nprecio: {self.precio}"
